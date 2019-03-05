f5086a861c98d717a6d7dd496579467ede4e0844
Nathan <flashiora@gmail.com>
2019-03-05 15:06:56 +0100

    Suppression d'un article [flobaudry-2]

---
+++ src/model/ArticleManager.java
@@ -0,0 +1,17 @@
+package model;
+
+import controller.Article;
+import java.util.ArrayList;
+
+public class ArticleManager {
+
+    public boolean delete_article(Article article) {
+        // TODO
+        return true;
+    }
+
+    public ArrayList<Article> load_user_feed(){
+        // TODO
+        return new ArrayList<>();
+    }
+}
