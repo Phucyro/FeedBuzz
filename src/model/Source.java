616f171129a10e1a8a96e2d843bc4283dd649368
Sonia <sonia.malki@ulb.ac.be>
2019-03-10 21:45:36 +0100

    ajout de la méthode download dans Source.java

--- src/model/Source.java
+++ src/model/Source.java
@@ -1,5 +1,10 @@
 package model;
 
+import controller.Article;
+import controller.ParserRss;
+
+import java.util.ArrayList;
+
 public class Source {
     private String source_name ;
     private boolean checked = true;
@@ -11,6 +16,15 @@ public class Source {
     public boolean  isChecked() { return checked; }
 
     public void download(int number,int lifespan){
+        source_name = getName();
+        ParserRss source = new ParserRss();
+        ArticleManager articleManager = new ArticleManager("./test.db");
+        ArrayList<Article> articles = source.parse(source_name);
+        Article article_temp;
+        for (int i = 0; i < number; i++) {
+            article_temp = articles.get(i);
+            articleManager.add_article(article_temp);
+        }
 
     }
 
