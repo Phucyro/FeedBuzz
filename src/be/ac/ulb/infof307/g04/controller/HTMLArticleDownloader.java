8b292daca5f10e7a944cc006fe1fb3cb569f2a7b
Nemael <panouillot.mael@gmail.com>
2019-04-20 17:00:37 +0200

    Push incomplet

--- src/be/ac/ulb/infof307/g04/controller/HTMLArticleDownloader.java
+++ src/be/ac/ulb/infof307/g04/controller/HTMLArticleDownloader.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -12,11 +13,13 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Random;
 
 
 public class HTMLArticleDownloader {
     //Recois une URL
     //Extrait tout les liens et les trie par type
+    private Article article;
     public HTMLArticleDownloader() {
     }
 
@@ -49,16 +52,32 @@ public class HTMLArticleDownloader {
     }
 
     public static String ArticleLocalifier(String url) throws IOException {
+        String foldername = sanitize_string(url);
+        new File("./media/"+ foldername).mkdir();
         Document doc = Jsoup.connect(url).get();
         Elements links = doc.select("a[href]");
         Elements media = doc.select("[src]");
         Elements imports = doc.select("link[href]");
 
         replaceLinksHref(links,"#");
-        replaceLinksSrc(media);
-        replaceImportsHref(imports);
-        System.out.println(doc);
+        replaceLinksSrc(media, foldername);
+        replaceImportsHref(imports, foldername);
+        //System.out.println(doc);
         return ("Ninja");
+    }
+
+    private static String sanitize_string(String url){
+        url = url.replace("/","");
+        url = url.replace("<","");
+        url = url.replace(">","");
+        url = url.replace(":","");
+        url = url.replace("\"","");
+        url = url.replace("/","");
+        url = url.replace("\\","");
+        url = url.replace("|","");
+        url = url.replace("?","");
+        url = url.replace("*","");
+        return url;
     }
 
     private static void replaceLinksHref(Elements links,String toReplace) {
@@ -68,20 +87,20 @@ public class HTMLArticleDownloader {
         }
     }
 
-    private static void replaceLinksSrc(Elements medias){
+    private static void replaceLinksSrc(Elements medias, String foldername){
         for (Element media: medias){
             try {
-                media.attr("src", downloader(media.attr("src")));
+                media.attr("src", downloader(media.attr("src"), foldername));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static void replaceImportsHref(Elements imports){
+    private static void replaceImportsHref(Elements imports, String foldername){
         for (Element _import: imports){
             try {
-                _import.attr("href", downloader(_import.attr("href")));
+                _import.attr("href", downloader(_import.attr("href"), foldername));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -89,14 +108,16 @@ public class HTMLArticleDownloader {
     }
 
 
-    private static String downloader(String url) throws IOException {
+    private static String downloader(String url, String foldername) throws IOException {
+        //System.out.println(url);
         InputStream in = new URL(url).openStream();
         String filename = getFilename(in, url);
-        
-        Files.copy(in, Paths.get("./media/" + filename), StandardCopyOption.REPLACE_EXISTING);
-        System.out.println(url);
-        System.out.println(Paths.get(filename).toString());
-        return Paths.get("./media/" + filename).toString();
+        String complete_path = "./media/" + foldername + "/" + filename;
+        Files.copy(in, Paths.get(complete_path), StandardCopyOption.REPLACE_EXISTING);)
+        //System.out.println(url);
+        //System.out.println(Paths.get(filename).toString());
+        System.out.println(complete_path);
+        return Paths.get(complete_path).toString();
     }
 
     private static String getFilename(InputStream in, String url){
