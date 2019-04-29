70ea71e47ef6f95ad639f121f91904b71367f645
ElzULB <aelzerho@ulb.ac.be>
2019-04-29 14:01:57 +0200

    Merge branch 'master' of /home/anass/Bureau/Génie/groupe04 with conflicts.

--- src/be/ac/ulb/infof307/g04/Main.java
+++ src/be/ac/ulb/infof307/g04/Main.java
@@ -18,319 +18,189 @@ import javafx.scene.control.Label;
 import javafx.scene.control.*;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
-
-/**
- * Class Main, used to lauch the application
- */
 
 
 public class Main extends Application {
 
     @FXML
-    private ListView<DatabaseArticle> listViewArticles;
-
+    private ListView<Article> list_view_articles;
     @FXML
-    private MenuItem quitButton;
-
+    private MenuItem quit_button;
     @FXML
-    private  GridPane gridPane;
+    private  GridPane grid_pane;
 
     private static ArticleManager article_manager;
     private static SourceManager source;
 
-    private ToolBar searchBar;
-    private Button closeSearchButton;
-    private TextField searchField;
+    private ToolBar search_bar;
+    private Button close_search_button;
+    private TextField search_field;
     private Label match_count;
 
-    @FXML
-    private MenuItem readArticleImage;
-    @FXML
-    private MenuItem searchArticleImage;
-    @FXML
-    private MenuItem copyArticleLinkImage;
-    @FXML
-    private MenuItem configureSourcesImage;
-    @FXML
-    private MenuItem configureTagsImage;
-    @FXML
-    private MenuItem exitAppImage;
-
-    private ArrayList<Stage> stageArrayList = new ArrayList<Stage>();
-
-    public static void main(String[] _args) {
+    public static void main(String[] args) {
         article_manager = new ArticleManager("./article_db", "password");
-        initDb();
         source = new SourceManager("./article_db");
-        launch(_args);
+        init_db();
+        launch(args);
     }
 
     @Override
-    public void start(Stage _primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/be/ac/ulb/infof307/g04/view/ArticleList.fxml"));
-
-        _primaryStage.setTitle("Article List");
+        primaryStage.setTitle("Fenêtre principale");
 
         Scene scene = new Scene(root);
-        _primaryStage.setScene(scene);
-        _primaryStage.show();
-        _primaryStage.setOnCloseRequest(e -> Platform.exit());
-
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+
     @FXML
-    public void initialize() throws IOException, ParserConfigurationException, SAXException, ParseException {
-        /*
-        Initialize the main window -> has a close button, a search bar and display all the articles in the DB
-         */
+    public void initialize() {
+        search_bar = new ToolBar();
+        search_bar.setPrefHeight(40.0);
+        search_bar.setPrefWidth(200.0);
+        GridPane.setConstraints(search_bar, 0, 0);
 
-
-        searchBar = new ToolBar();
-            searchBar.setPrefHeight(40.0);
-            searchBar.setPrefWidth(200.0);
-            GridPane.setConstraints(searchBar, 0, 0);
-            closeSearchButton = new Button("Close");
-            closeSearchButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    changeSearchBarStatus();
-                }
-            });
-            searchField = new TextField();
-            searchField.textProperty().addListener(new ChangeListener<String>() {
-                @Override
-                public void changed(ObservableValue<? extends String> observable,
-                                    String oldValue, String newValue) {
-
-                    ArrayList<DatabaseArticle> articles = article_manager.loadArticles(newValue);
-                    displayArticles(articles);
-                    match_count.setText(articles.size() + " matches");
-                }
-            });
-            match_count = new Label();
-            searchBar.getItems().addAll(closeSearchButton, searchField, match_count);
-
-            listViewArticles.setCellFactory(lv -> new ArticleCell());
-            quitButton.setOnAction(e -> Platform.exit());
-            try{
-                source.download(article_manager);
-            } catch (IOException e){
-                //Cas hors connexion
+        close_search_button = new Button("Close");
+        close_search_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                change_search_bar_status();
             }
-            try {
-                article_manager.verifyArticles();
-            } catch (Exception e){
-                //Ne rien faire car le logiciel est lance en hors-ligne
+        });
+
+        search_field = new TextField();
+        search_field.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable,
+                                String oldValue, String newValue) {
+
+                ArrayList<Article> articles = article_manager.load_articles(newValue);
+                display_articles(articles);
+                match_count.setText(articles.size() + " matches");
             }
+        });
 
-            displayArticles(article_manager.loadArticles());
+        match_count = new Label();
+        search_bar.getItems().addAll(close_search_button, search_field, match_count);
 
-            setHelpImages();
-    }
-
-    private void setImage(String s, int i, int i2, MenuItem readArticleImage) {
-        ImageView readIcon = new ImageView(new Image(s));
-        readIcon.setFitHeight(i);
-        readIcon.setFitWidth(i2);
-        readArticleImage.setGraphic(readIcon);
-    }
-    private void setHelpImages() {
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/ReadArticle.png", 250, 400, readArticleImage);
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/SearchByTitle.png", 280, 380, searchArticleImage);
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/CopyToClipboard.png", 250, 400, copyArticleLinkImage);
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/ConfigureSources.png", 380, 530, configureSourcesImage);
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/ConfigureTags.png", 350, 550, configureTagsImage);
-        setImage("/be/ac/ulb/infof307/g04/pictures/Help_Pictures/Exit.png", 200, 350, exitAppImage);
-    }
-
-
-    @FXML
-    public void displayArticles(ArrayList<DatabaseArticle> _articles) {
-        /**
-         * Display all the valid _articles in the window
-         * @param _articles
-         *              _articles that haven't been deleted in the DB
-         */
-        listViewArticles.getItems().clear();
-        for (DatabaseArticle item : _articles) {
-            listViewArticles.getItems().add(item);
-        }
-    }
-
-    public void disconnectUser() {
-        Platform.exit();
-        for (int i = 0; i < stageArrayList.size(); i++) {
-            stageArrayList.get(i).close();
-        }
-        Stage stage = (Stage) gridPane.getScene().getWindow();
-        stage.close();
-
-    }
-    public void relaunchApplication() throws Exception {
-        disconnectUser();
-        Main mymain = new Main();
-        mymain.start(new Stage());
-
+        list_view_articles.setCellFactory(lv -> new ArticleCell());
+        quit_button.setOnAction(e -> Platform.exit());
+        source.download(article_manager);
+        article_manager.verify_articles();
+        display_articles(article_manager.load_articles());
     }
 
     @FXML
-    private void openArticleWindow() {
-        /**
-         * Method that opens an article when the user click on it
-         * @throws Exception : when no article has been selected
-         */
+    public void display_articles(ArrayList<Article> articles) {
+        // La fonction rafraîchit la fenêtre principale (articles supprimés/ rajoutés)
+        list_view_articles.getItems().setAll(articles);
+    }
+
+
+
+    @FXML
+    private void open_article_window() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/be/ac/ulb/infof307/g04/view/ViewSingleArticle.fxml"));
-            DatabaseArticle articleToRead = listViewArticles.getSelectionModel().getSelectedItem();
-            ViewSingleArticle controller = new ViewSingleArticle(articleToRead);
+            ViewSingleArticle controller = new ViewSingleArticle(list_view_articles.getSelectionModel().getSelectedItem());
+            System.out.println("Ouverture de l'article");
             loader.setController(controller);
-            controller.setArticlesWindows(this);
+            controller.set_articles_windows(this);
             Parent root = (Parent) loader.load();
             Stage stage = new Stage();
-            stage.setTitle(articleToRead.getTitle());
             stage.setScene(new Scene(root));
             stage.show();
-            stageArrayList.add(stage);
-
-        } catch(NullPointerException e){
-            showErrorBox("No article selected");
-            e.printStackTrace();
-        }catch(ParserConfigurationException e){
-            showErrorBox("Parser configuration error");
-        }catch(IOException e){
-            showErrorBox("No article selected");
-            e.printStackTrace();
-        }catch(SAXException e){
-            showErrorBox("SAX Error");
-        } catch (ParseException e) {
-            showErrorBox("Parse error");
+        } catch (Exception e) {
+            System.out.println("Aucun article n'a été sélectionné");
         }
     }
 
-    private void showErrorBox(String s) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText(s);
-
-        alert.showAndWait();
-    }
-
     @FXML
-    private void copyLinkToClipboard() {
-        /**
-         * copy the link of the article
-         * @throws Exception : when no article has been selected
-         */
+    private void copy_link_to_clipboard() {
         try {
-            String myString = listViewArticles.getSelectionModel().getSelectedItem().getLink();
+            String myString = list_view_articles.getSelectionModel().getSelectedItem().getLink();
             StringSelection stringSelection = new StringSelection(myString);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(stringSelection, null);
         } catch (Exception e) {
-            showErrorBox("Error while copying link to clipboard");
-        }
-    }
-
-    public void openWindow(FXMLLoader loader, String title, String window_title){
-        try {
-            Parent root = (Parent) loader.load();
-
-            Stage stage = new Stage();
-            stage.setTitle(title);
-            stage.setScene(new Scene(root));
-            stage.show();
-        }
-        catch (Exception e) {
-            showErrorBox("Error while opening "+ window_title + " window!");
+            System.out.println("Veuillez selectionner un article");
         }
     }
 
     @FXML
-    public void openSourceWindow(ActionEvent _actionEvent) {
-        /**
-         * Opens the sources window
-         * @throws Exception
-         */
+    public void open_source_window(ActionEvent actionEvent) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/be/ac/ulb/infof307/g04/view/SourceMenu.fxml"));
             SourceMenu controller = new SourceMenu();
             loader.setController(controller);
-            openWindow(loader, "Choose sources", "source");
+            Parent root = (Parent) loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+
         } catch (Exception e) {
-            showErrorBox("Error while opening source window!");
+            e.printStackTrace();
         }
     }
 
-    public void openTagWindow(ActionEvent _actionEvent) {
-        /*
-        Open the tag window
-         */
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/be/ac/ulb/infof307/g04/view/TagMenu.fxml"));
-            TagMenu controller = new TagMenu();
-            loader.setController(controller);
-            openWindow(loader, "Manage tags", "tag");
-        } catch (Exception e) {
-            showErrorBox("Error while opening the tag window!");
-        }
-    }
     @FXML
-    public void changeSearchBarStatus(){
-        /*
-        If the search bar is available or not. When you close it, it loads all the articles again
-         */
-        if (gridPane.getChildren().indexOf(searchBar) == -1) {
-            gridPane.getChildren().add(searchBar);
+    public void change_search_bar_status(){
+        if (grid_pane.getChildren().indexOf(search_bar) == -1) {
+            grid_pane.getChildren().add(search_bar);
             match_count.setText("");
         } else {
-            gridPane.getChildren().remove(searchBar);
-            displayArticles(article_manager.loadArticles());
+            grid_pane.getChildren().remove(search_bar);
+            display_articles(article_manager.load_articles());
         }
     }
 
-    private static void initDb() {
-        /*
-        Initialize all the tags and the sources
-         */
-        initTags();
-        initSources();
+    private static void init_db() {
+        init_tags();
+        init_sources();
     }
 
-    private static void initTags() {
-        /*
-        Initialize all the tags
-         */
+    private static void init_tags() {
         String[] tags = {"Business", "Default", "Entertainment", "Health", "Science", "Sports", "Technology"};
         TagManager tagManager = new TagManager("./article_db", "password");
         DatabaseTag tag = new DatabaseTag();
         for(int i = 0; i < tags.length; i++){
             tag.setName(tags[i]);
-            tagManager.addTag(tag);
+            tagManager.add_tag(tag);
         }
     }
 
-    private static void initSources() {
-        /*
-        Initialize all the sources
-         */
+    private static void init_sources() {
         SourceManager sourceManager = new SourceManager("./article_db");
         ArrayList<DatabaseSource> sources = new ArrayList<>();
         sources.add(new DatabaseSource("The Verge", "https://www.theverge.com/rss/index.xml", "Technology"));
         sources.add(new DatabaseSource("BBC world news", "http://feeds.bbci.co.uk/news/world/rss.xml"));
-        sources.forEach(sourceManager::addSource);
+        sources.forEach(sourceManager::add_source);
     }
+
+    public void open_tag_window(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/be/ac/ulb/infof307/g04/view/TagMenu.fxml"));
+        TagMenu controller = new TagMenu();
+        loader.setController(controller);
+        Parent root = (Parent) loader.load();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
 };
